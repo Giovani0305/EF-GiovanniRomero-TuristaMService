596d31c5b3cce69fc5087317ac00c99d1cfc0256
Giovani0305 <116698863+Giovani0305@users.noreply.github.com>
2022-12-14 08:08:49 -0500

    Add files via upload

---
+++ TURISTA.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO dbhoteleria.turista VALUES (1,'Calle Madrid 1236','Sergio Ramos','953344118');
+INSERT INTO dbhoteleria.turista VALUES (2,'Avenida Bolivar 7855','Leonel Messi','903040506');
+INSERT INTO dbhoteleria.turista VALUES (3,'Jirón Camana 7855','Juan Reynoso','923022116');
